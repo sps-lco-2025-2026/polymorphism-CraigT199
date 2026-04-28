f6d2d784c6a8685a2d59cf01a9aa956fc4178f0e
CraigT199 <craigt@stpaulsschool.org.uk>
2026-04-28 09:03:14 +0100

    person class

---
+++ InheritanceTasks.Test/Test1.cs
@@ -0,0 +1,14 @@
+﻿using InheritanceTask.Lib;
+
+namespace InheritanceTasks.Test;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        var person = new Person("Toby", "Craig", "2009-06-02");
+        Assert.IsTrue(person.Valid);
+    }
+}
